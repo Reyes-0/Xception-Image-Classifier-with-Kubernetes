ffe09cb4c30dae8f0b3856e6f185ad2f1b5b2797
usernameistakenn <157951563+usernameistakenn@users.noreply.github.com>
2024-08-12 13:52:14 +0800

    Deleting files

--- model_training/model_training.py
+++
@@ -1,3 +0,0 @@
-# U ONLY NEED ONE MODEL CHOOSE THE MODEL WITH THE BEST( MOST LIKELY THE XCEPTION MODEL)
-# Need to take the raw data from the files, and then train, also need a variable assigned to the model to do inference 
-# like u finished training the model (trained_model = model.train()) so u assign it to a variable and the inference.py from import that trained_model just like CAIE
