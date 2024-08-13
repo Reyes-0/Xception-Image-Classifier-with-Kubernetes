b8841002fa3e6703cb55f0736327daf8f2472265
ShaunWee <weesws48@gmail.com>
2024-08-13 12:24:22 +0800

    model inference code v5

--- model_inference/model_inference.py
+++ model_inference/model_inference.py
@@ -7,33 +7,31 @@ import os
 import numpy as np
 from tensorflow.keras.models import load_model
 import cv2
-from parameters import model_save_dir
+from parameters import model_save_dir, prediction_save_dir
 
-class ModelInference:
-    def __init__(self, model_path, class_names):
-        self.model = load_model(model_path)
-        self.class_names = class_names
-
-    def predict(self, image_path):
-        # Load the resized image
-        resized_img = np.load(image_path)
-
-        # Make predictions
-        predictions = self.model.predict(resized_img.reshape((1, 71, 71, 3)))
-
-        # Get the predicted class index
-        predicted_class_index = np.argmax(predictions)
-
-        # Get the predicted class name
-        predicted_class_name = self.class_names[predicted_class_index]
-
-        return predicted_class_name
-
-# Example usage:
+# Load the saved model
 model_path = os.path.join(model_save_dir, "xception_model.h5")
-class_names = {0: 'airplane', 1: 'automobile', 2: 'ship', 3: 'truck'}
-image_path = os.path.join('data/processed_uploaded_image', 'resized__uploaded_image.npy')
+model = load_model(model_path)
 
-inference = ModelInference(model_path, class_names)
-predicted_class_name = inference.predict(image_path)
+# Load the class names
+class_names = {0: 'airplane', 1: 'automobile', 2: 'ship', 3: 'truck'}
+
+# Load the resized image
+image_path = os.path.join('data/processed_uploaded_image', 'resized__uploaded_image.npy')
+resized_img = np.load(image_path)
+
+# Make predictions
+predictions = model.predict(resized_img)
+
+# Get the predicted class index
+predicted_class_index = np.argmax(predictions)
+
+# Get the predicted class name
+predicted_class_name = class_names[predicted_class_index]
+
 print(f"Predicted class: {predicted_class_name}")
+
+# Save the predicted class to a file
+prediction_file_path = os.path.join(prediction_save_dir, "predicted_class.txt")
+with open(prediction_file_path, "w") as f:
+    f.write(predicted_class_name) # writes the predicted class name in the file
--- model_inference/parameters.py
+++ model_inference/parameters.py
@@ -1,2 +1,3 @@
 # File directories for image testing
 model_save_dir = '/saved_model'
+prediction_save_dir = '/predictions'
